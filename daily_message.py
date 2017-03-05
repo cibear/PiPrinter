5c2898ee59ac89712fd4141d4c607c7fca370daf
cibear <holo-deck@gmx.de>
2017-03-05 16:52:22 +0100

    fixing

--- daily_message.py
+++ daily_message.py
@@ -10,21 +10,24 @@ printer = Adafruit_Thermal("/dev/serial0", 19200, timeout=5)
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 item_folder = dir_path + '/items'
+#item_folder = "C:/Users/Christian/Documents/GitHub/PiPrinter/items"
 
 print "Script started..."
 
-#printer.setSize('L')   # Set type size, accepts 'S', 'M', 'L'
+printer.setSize('L')   # Set type size, accepts 'S', 'M', 'L'
 #printer.println(filewalker.pick_random_pic(item_folder))
 picked_item = filewalker.pick_item(item_folder,date.today())
 
 #process according to file type
 print "picked item: " + picked_item
-print "png?" + picked_item.rsplit(".",1)[0] == "png"
-print "txt?" + picked_item.rsplit(".",1)[0] == "txt"
+print picked_item.rsplit(".",1)[1]
+print "png?" + str(picked_item.rsplit(".",1)[1] == "png")
+print "txt?" + str(picked_item.rsplit(".",1)[1] == "txt")
 
-if picked_item.rsplit(".",1)[0] == "png":
+if picked_item.rsplit(".",1)[1] == "png":
     printer.printImage(Image.open(filewalker.pick_item(item_folder)), True)
-elif picked_item.rsplit(".",1)[0] == "txt":
+    print "Image printed..."
+elif picked_item.rsplit(".",1)[1] == "txt":
     textfile_handle = open(picked_item, "r") 
     print textfile_handle.read()
     printer.println(textfile_handle.read()) 
