bf71d0b5b576046df92cdaeef1263dcbaffcd969
cibear <holo-deck@gmx.de>
2017-03-05 16:08:03 +0100

    bugfix

--- daily_message.py
+++ daily_message.py
@@ -11,6 +11,8 @@ printer = Adafruit_Thermal("/dev/serial0", 19200, timeout=5)
 dir_path = os.path.dirname(os.path.realpath(__file__))
 item_folder = dir_path + '/items'
 
+print "Script started..."
+
 #printer.setSize('L')   # Set type size, accepts 'S', 'M', 'L'
 #printer.println(filewalker.pick_random_pic(item_folder))
 picked_item = filewalker.pick_item(item_folder,date.today())
@@ -20,7 +22,7 @@ if picked_item.rsplit(".",1)[0] == "png":
     printer.printImage(Image.open(filewalker.pick_item(item_folder)), True)
 elif picked_item.rsplit(".",1)[0] == "txt":
     textfile_handle = open(picked_item, "r") 
-    #print textfile_handle.read()
+    print textfile_handle.read()
     printer.println(textfile_handle.read()) 
 printer.feed(3)
 #clean up by moving picked item to bin folder
